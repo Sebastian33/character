767757bf15981b8ac7ba59dae21002cffc95df90
Бахтигозин Всеволод <Vsevolod.Bakhtigozin@avtor.ua>
2018-09-27 18:07:54 +0300

    not tested

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,17 @@
 #include "Header.h"
 
+
 double LinPotential(const permutation &prm, u64 a, u64 b, unsigned n)
 {
-	for (unsigned x = 1; x < (1 << n); x++)
+	const int size(1 << n);
+	const int mod(size - 1);
+	const double pi = std::acos(-1);
+	std::complex<double> sum(0, 0);
+	const std::complex<double> i(0, 1);
+	const std::complex<double> e = std::exp(i*pi * 2.0 / (double)size); 
+	for (unsigned x = 1; x < size; x++)
 	{
-
+		sum += std::pow(e, a*x & mod)*std::conj(std::pow(e, b*prm[x] & mod));
 	}
+	return std::abs(sum);
 }
