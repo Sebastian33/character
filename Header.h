1c594caafa2b4c84ff6bf25ece00afb0549ea6e7
Бахтигозин Всеволод <Vsevolod.Bakhtigozin@avtor.ua>
2018-11-15 13:34:58 +0200

    init

--- Header.h
+++ Header.h
@@ -1,8 +1,11 @@
 #pragma once
 #include <complex>
 #include <vector>
-
+//TODO: make as a class
 using u64 = unsigned long long;
 using permutation = std::vector<unsigned long long>;
+using cmplx = std::complex<double>;
 
 double LinPotential(const permutation &prm, u64 a, u64 b, unsigned n);
+permutation generatePermutation(unsigned n);
+void experiment(const permutation &prm, unsigned n);
--- Source.cpp
+++ Source.cpp
@@ -1,17 +1,78 @@
 #include "Header.h"
-
+#include <random>
+#include <fstream>
 
 double LinPotential(const permutation &prm, u64 a, u64 b, unsigned n)
 {
 	const int size(1 << n);
 	const int mod(size - 1);
 	const double pi = std::acos(-1);
-	std::complex<double> sum(0, 0);
-	const std::complex<double> i(0, 1);
-	const std::complex<double> e = std::exp(i*pi * 2.0 / (double)size); 
-	for (unsigned x = 1; x < size; x++)
+	cmplx sum(0, 0);
+	const cmplx e = std::exp(cmplx(0, 1) * pi * 2.0 / (double)size); 
+	for (unsigned x = 0; x < size; x++)
 	{
-		sum += std::pow(e, a*x & mod)*std::conj(std::pow(e, b*prm[x] & mod));
+		sum += std::pow(e, a*x & mod) * std::conj(std::pow(e, b*prm[x] & mod));
 	}
-	return std::abs(sum);
+	return pow(std::abs(sum), 2);
+}
+
+permutation generatePermutation(unsigned n)
+{
+	const int size(1 << n);
+	std::mt19937_64 engine;
+	std::uniform_int_distribution<int> dstrb;
+	permutation prm(size);
+
+	for (int i = 0; i < size; i++)
+	{
+		prm[i] = i;
+	}
+
+	for (int i = size - 1; i > 1; i--)
+	{
+		std::swap(prm[i], prm[dstrb(engine) % i]);
+	}
+	return prm;
+}
+
+void experiment(const permutation &prm, unsigned n)
+{
+	const int size(1 << n);
+	const int mod(size - 1);
+	const double pi = std::acos(-1);
+	cmplx sum(0, 0);
+	const cmplx e = std::exp(cmplx(0, 1) * pi * 2.0 / (double)size);
+	std::vector<std::vector<double>> data(size, std::vector<double>(size, 0));
+
+	for (int a = 1; a < size; a++)
+	{
+		for (int b = 1; b < size; b++)
+		{
+			for (unsigned x = 0; x < size; x++)
+			{
+				//sum += std::pow(e, a*x & mod) * std::conj(std::pow(e, b*prm[x] & mod));
+				sum += std::pow(e, (a*x - b * prm[x]) & mod); //better, faster, stronger ...
+			}
+			data[a - 1][b - 1] = std::norm(sum);
+			sum = 0;
+		}
+	}
+
+	std::ofstream out("data.txt");
+	for (int i = 1; i < size; i++)
+	{
+		out << ' ' << i;
+	}
+	out << std::endl;
+
+	for (int a = 1; a < size; a++)
+	{
+		out << a << ' ';
+		for (int b = 1; b < size; b++)
+		{
+			out << data[a - 1][b - 1] << ' ';
+		}
+		out << std::endl;
+	}
+	out.close();
 }
